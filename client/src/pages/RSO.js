46d9284945fa27552c2f48af5d26da9c6c94ec7c
BunnyCorruption <huntercharriman@gmail.com>
2022-04-08 21:23:32 -0400

    finished api for RSO, and changed logos

--- client/src/pages/RSO.js
+++ client/src/pages/RSO.js
@@ -1,86 +1,91 @@
-import React, { } from "react";
+import React from "react";
 import { Card, Button, Navbar, Container } from "react-bootstrap";
-import Axios from 'axios';
-
+import Axios from "axios";
 
 export default class RSO extends React.Component {
-  
+  schoolName;
   state = {
-    schools: []
-  }
-
-  componentDidMount(){
-    Axios.get('http://localhost:3001/api/get/rso/').then(res => {
-      console.log(res.data);
-      const schools = res.data;
-      this.setState({schools});
-    })
+    schools: [],
   };
 
-  suggestRSO(){
-    console.log('ye');
-    Axios.post('http://localhost:3001/api/post/rso/')
+  componentDidMount() {
+    Axios.get("http://localhost:3001/api/get/rso/").then((res) => {
+      //console.log(res.data);
+      const schools = res.data;
+      this.setState({ schools });
+    });
   }
 
-render(){
-  return (
-    <>
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#rso">
-            <img
-              alt=""
-              src="/pngwing.com.png"
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />{" "}
-            Calend-R-U-Coming?
-          </Navbar.Brand>
-          <div>
-            <Button type="button" className="pull-right">
-              Create an Event
-            </Button>
-            <Button type="button" className="mx-4 pull-right">
-              Join an RSO
-            </Button>
-            <Button type="button" className="pull-right btn btn-warning">
-              Logout
-            </Button>
-          </div>
-        </Container>
-      </Navbar>
-      <div className="bg">
-        <Container
-          className="d-flex align-items-center justify-content-center"
-          style={{ minHeight: "100vh" }}
-        >
-          <Card style={{ width: 800 }}>
-            <Card.Body>
-              <h2 className="text-center mb-4">RSO</h2>
-              <ul className="list-group">
-                {this.state.schools.map((listitem) => (
-                  <li
-                    className="list-group-item list-group-item-primary p-2"
-                    key={listitem.rsoid}
-                  >
-                    <div className="d-flex justify-content-between">
-                      <div>
-                      {listitem.rName} 
+  suggestRSO() {
+    Axios.post("http://localhost:3001/api/post/rso/", { newrso: 'e' }).then(
+      (res) => {
+        //console.log(res)
+      },
+      (err) =>{
+        //console.log(err)
+      }
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <Navbar bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#rso">
+              <img
+                alt=""
+                src="/pngwing.com.png"
+                width="30"
+                height="30"
+                className="d-inline-block align-top"
+              />{" "}
+              Calend-R-U-Coming?
+            </Navbar.Brand>
+            <div>
+              <Button type="button" className="pull-right">
+                Create an Event
+              </Button>
+              <Button type="button" className="mx-4 pull-right">
+                Join an RSO
+              </Button>
+              <Button type="button" className="pull-right btn btn-warning">
+                Logout
+              </Button>
+            </div>
+          </Container>
+        </Navbar>
+        <div className="bg">
+          <Container
+            className="d-flex align-items-center justify-content-center"
+            style={{ minHeight: "100vh" }}
+          >
+            <Card style={{ width: 800 }}>
+              <Card.Body>
+                <h2 className="text-center mb-4">
+                  Registered Student Organizations
+                </h2>
+                <ul className="list-group">
+                  {this.state.schools.map((listitem) => (
+                    <li
+                      className="list-group-item list-group-item-primary p-2"
+                      key={listitem.rsoid}
+                    >
+                      <div className="d-flex justify-content-between">
+                        <div>{listitem.rName}</div>
+                        <Button>Join</Button>
                       </div>
-                      <Button >Join</Button>
-                    </div>
-
-
-                  </li>
-                ))}
-              </ul>
-              <Button onClick={this.suggestRSO} className="w-100 mt-4">Return</Button>
-            </Card.Body>
-          </Card>
-        </Container>
-      </div>
-    </>
-  );
-}
+                    </li>
+                  ))}
+                </ul>
+                <Button className="w-100 mt-4" onClick={this.suggestRSO}>
+                  Return
+                </Button>
+              </Card.Body>
+            </Card>
+          </Container>
+        </div>
+      </>
+    );
+  }
 }
--- server/index.js
+++ server/index.js
@@ -1,175 +1,162 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const mysql = require('mysql');
-const dotenv = require('dotenv');
+const mysql = require("mysql");
+const dotenv = require("dotenv");
 
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const saltRounds = 10;
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 // JWT
-const jwt = require('jsonwebtoken'); // WHALE
+const jwt = require("jsonwebtoken"); // WHALE---------------
 
 //WHALE
 const verifyJWT = (req, res, next) => {
-    const authHeader = req.headers["x-access-token"]
-    const token = authHeader && authHeader.split('')[1]
-    if (!token) {
-        res.send("Ripperino. No token.");
-    } else {
-        jwt.verify(token, process.env.SECRET, (err, decoded) => {
-            if (err) {
-                res.json({auth: false, message: "FAILURE"});
-            } else {
-                req.userId = decoded.uid;
-                next();
-            }
-        });
-    }
+  const authHeader = req.headers["x-access-token"];
+  const token = authHeader && authHeader.split("")[1];
+  if (!token) {
+    res.send("Ripperino. No token.");
+  } else {
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err) {
+        res.json({ auth: false, message: "FAILURE" });
+      } else {
+        req.userId = decoded.uid;
+        next();
+      }
+    });
+  }
 };
 
-dotenv.config({path: './.env'});
+dotenv.config({ path: "./.env" });
 
 const db = mysql.createConnection({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE,
 });
 
-
-db.connect(function(err) {
-    if (err) {
-      console.error('Database connection failed: ' + err.stack);
-      return;
-    };
-    console.log('Connected to database.');
+db.connect(function (err) {
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("Connected to database.");
 });
 
 app.use(express.json());
-app.use(cors({
-    origin:["http://localhost:3000"],
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST"],
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 app.use(cookieParser());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
+app.use(
+  session({
     key: "userID",
     secret: "subscribe",
     resave: false,
     saveUninitialized: false,
     cookie: {
-        expires: 60 * 60 * 24,
+      expires: 60 * 60 * 24,
     },
-}));
+  })
+);
 
+app.post("/api/register", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-app.post('/api/register', (req, res)=>{
-    
-    const username = req.body.username
-    const password = req.body.password
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    if (err) {
+      console.log(err);
+    }
 
-    bcrypt.hash(password, saltRounds, (err, hash) => {
-        if (err) {
-            console.log(err)
-        }
-
-        db.query(
-            "INSERT INTO User (username, password) VALUES (?,?)",
-            [username, hash],
-            (err, result) => {
-                console.log(err);
-            }
-        );     
-    });
-     
+    db.query(
+      "INSERT INTO User (username, password) VALUES (?,?)",
+      [username, hash],
+      (err, result) => {
+        console.log(err);
+      }
+    );
+  });
 });
 
 // WHALE
 app.get("/api/isUserAuth", verifyJWT, (req, res) => {
-    res.send("work in progress")
-    // res.json(db.filter(post => post.username === req.user.name))
+  res.send("work in progress");
+  // res.json(db.filter(post => post.username === req.user.name))
 });
 
 app.get("/api/login", (req, res) => {
-    if (req.session.user) {
-        res.send({loggedIn: true, user: req.session.user});
-    } else {
-        res.send({ loggedIn: false});
+  if (req.session.user) {
+    res.send({ loggedIn: true, user: req.session.user });
+  } else {
+    res.send({ loggedIn: false });
+  }
+});
+
+app.post("/api/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  db.query("SELECT * FROM User WHERE username = ?", username, (err, result) => {
+    if (err) {
+      res.send({ err: err });
     }
-});
 
-app.post('/api/login', (req, res)=>{
-    
-    const username = req.body.username;
-    const password = req.body.password;
+    if (result.length > 0) {
+      bcrypt.compare(password, result[0].password, (error, response) => {
+        if (response) {
+          const id = result[0].uid; // WHALE
+          //WHALE
+          const token = jwt.sign({ id }, process.env.SECRET, {
+            expiresIn: 300,
+          });
 
-    db.query(
-        "SELECT * FROM User WHERE username = ?",
-        username,
-        (err, result) => {
-            if (err) {
-                res.send({err: err});
-            } 
-            
-            if (result.length > 0) {
-                bcrypt.compare(password, result[0].password, (error, response) => {
-                    if (response) {
-                        const id = result[0].uid; // WHALE
-                        //WHALE
-                        const token = jwt.sign({id}, process.env.SECRET, { 
-                            expiresIn: 300,     
-                        })
-
-                        req.session.user = result; // WHALE
-                        res.json({auth: true, token: token, result: result}); // passing every field from user table WHALE
-                    } else {
-                        res.json({auth: false, message: "Invalid Username/Password"});
-                    }
-                })
-            } else {
-                res.json({auth: false, message: "User doesn't exist"});
-            }
-            
+          req.session.user = result; // WHALE
+          res.json({ auth: true, token: token, result: result }); // passing every field from user table WHALE
+        } else {
+          res.json({ auth: false, message: "Invalid Username/Password" });
         }
-    );      
+      });
+    } else {
+      res.json({ auth: false, message: "User doesn't exist" });
+    }
+  });
 });
 
-app.get('/api/get', (req, res)=>{
-
-    db.query("SELECT * FROM User", (err, result)=>{
-            console.log(err);
-            console.log(result);
-    });
-    
+app.get("/api/get", (req, res) => {
+  db.query("SELECT * FROM User", (err, result) => {
+    console.log(err);
+    console.log(result);
+  });
 });
 
-app.get('/api/get/rso', (req, res) =>{
+app.get("/api/get/rso", (req, res) => {
+  db.query("SELECT * FROM RSO", (err, result) => {
+    //console.log(result);
+    return res.json(result);
+  });
+});
 
-     db.query("SELECT * FROM RSO", (err, result)=>{
-        console.log(result);
-        return res.json(result);
-    })
-    
-})
-
-app.post('/api/post/rso', (req, res) =>{
-
-    const schoolName = req.body.newrso
-
-    db.query("INSERT INTO RSO (rName) VALUES (?)", schoolName, (err, result)=>{
-       console.log(err);
-   })
-   
-})
+app.post("/api/post/rso", (req, res) => {
+  const schoolName = req.body.newrso;
+  //console.log(schoolName);
+  const sqlInsert = `INSERT INTO RSO (rName) VALUES ('${schoolName}')`;
+  db.query(sqlInsert, (err, result) => {});
+});
 app.listen(3001, () => {
-    console.log('runnin on port 3001');
+  console.log("runnin on port 3001");
 });
